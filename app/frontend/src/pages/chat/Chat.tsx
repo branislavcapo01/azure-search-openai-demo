1ddfd151e2fb3ea7395c8cd93b307b4b0d5a07a7
branislavcapo01 <118799605+branislavcapo01@users.noreply.github.com>
2023-06-29 19:35:24 +0000

    ok

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -134,9 +134,9 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            
+                            <h1 className={styles.chatEmptyStateTitle}>Rozprávaj sa so svojimi údajmi</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Spýtaj sa alebo vyskúšaj príklad</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
